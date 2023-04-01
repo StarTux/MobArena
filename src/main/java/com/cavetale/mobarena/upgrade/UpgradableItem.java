0c82dcc9d0970dd77a4ec68a18063e8245c5b239
StarTux <ths.loy@googlemail.com>
2023-04-01 19:23:18 +0200

    Remove item repair; update enchantment cost; add certain incompatible enchants

--- src/main/java/com/cavetale/mobarena/upgrade/UpgradableItem.java
+++ src/main/java/com/cavetale/mobarena/upgrade/UpgradableItem.java
@@ -3,9 +3,9 @@ package com.cavetale.mobarena.upgrade;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
+import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.Damageable;
 
 @Getter
 public final class UpgradableItem {
@@ -17,28 +17,39 @@ public final class UpgradableItem {
         this.itemStack = itemStack;
         this.level = level;
         for (Enchantment enchantment : Enchantment.values()) {
-            if (!enchantment.canEnchantItem(itemStack)) continue;
+            final boolean incompatible;
+            if (enchantment.isCursed()) {
+                continue;
+            } else if (itemStack.getType() == Material.CROSSBOW && enchantment.equals(Enchantment.ARROW_INFINITE)) {
+                incompatible = true;
+            } else if (itemStack.getType() == Material.CROSSBOW && enchantment.equals(Enchantment.ARROW_FIRE)) {
+                incompatible = true;
+            } else if (!enchantment.canEnchantItem(itemStack)) {
+                continue;
+            } else {
+                incompatible = false;
+            }
             final int oldLevel = itemStack.getEnchantmentLevel(enchantment);
-            int conflictingLevel = 0;
+            if (oldLevel >= enchantment.getMaxLevel()) {
+                continue;
+            }
+            int totalConflictingLevel = 0;
             boolean conflicts = false;
-            if (itemStack.getEnchantmentLevel(enchantment) == 0) {
-                for (Enchantment oldEnchant : Enchantment.values()) {
-                    if (enchantment == oldEnchant) continue;
-                    conflictingLevel = itemStack.getEnchantmentLevel(oldEnchant);
-                    if (conflictingLevel > 0 && enchantment.conflictsWith(oldEnchant)) {
-                        conflicts = true;
-                    }
+            INNER: for (Enchantment oldEnchant : Enchantment.values()) {
+                if (enchantment.equals(oldEnchant)) continue INNER;
+                int conflictingLevel = itemStack.getEnchantmentLevel(oldEnchant);
+                if (conflictingLevel > 0 && enchantment.conflictsWith(oldEnchant)) {
+                    totalConflictingLevel += conflictingLevel;
+                    conflicts = true;
                 }
             }
-            if (!enchantment.isCursed() && oldLevel < enchantment.getMaxLevel()) {
-                final int requiredLevel = conflicts
-                    ? oldLevel + conflictingLevel + 9
-                    : oldLevel + 1;
-                upgrades.add(new EnchantmentUpgrade(enchantment, oldLevel + 1, requiredLevel));
+            int requiredLevel = oldLevel + 1;
+            if (conflicts) {
+                requiredLevel += 9 + totalConflictingLevel;
+            } else if (incompatible) {
+                requiredLevel += 10;
             }
-        }
-        if (itemStack.getItemMeta() instanceof Damageable damageable && damageable.getDamage() > 1) {
-            upgrades.add(new RepairUpgrade(damageable.getDamage()));
+            upgrades.add(new EnchantmentUpgrade(enchantment, oldLevel + 1, requiredLevel));
         }
     }
 
